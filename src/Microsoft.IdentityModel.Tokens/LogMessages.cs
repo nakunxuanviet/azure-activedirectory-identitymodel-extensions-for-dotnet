a12ded79c1f0b5ffa2bc2eef6e0cca91b40528e6
Roja Ennam <roennam@microsoft.com>
2020-09-18 13:10:56 -0700

    De-duplication

--- src/Microsoft.IdentityModel.Tokens/LogMessages.cs
+++ src/Microsoft.IdentityModel.Tokens/LogMessages.cs
@@ -128,6 +128,7 @@ namespace Microsoft.IdentityModel.Tokens
         public const string IDX10616 = "IDX10616: Encryption failed. EncryptionProvider failed for: Algorithm: '{0}', SecurityKey: '{1}'. See inner exception.";
         public const string IDX10617 = "IDX10617: Encryption failed. Keywrap is only supported for: '{0}', '{1}' and '{2}'. The content encryption specified is: '{3}'.";
         public const string IDX10618 = "IDX10618: Key unwrap failed using decryption Keys: '{0}'.\nExceptions caught:\n '{1}'.\ntoken: '{2}'.";
+        public const string IDX10619 = "IDX10619: Unable to obtain a CryptoProviderFactory, both EncryptingCredentials.CryptoProviderFactory and EncryptingCredentials.Key.CrypoProviderFactory are both null.";
 
         // Formating
         public const string IDX10400 = "IDX10400: Unable to decode: '{0}' as Base64url encoded string.";
